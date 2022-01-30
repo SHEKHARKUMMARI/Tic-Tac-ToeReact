974ad7ddf0ffed326fdc5ee230f943e60c59d788
SHEKHAR KUMMARI <74283917+SHEKHARKUMMARI@users.noreply.github.com>
2022-01-31 00:55:17 +0530

    Initial commit

---
+++ src/winner.js
@@ -0,0 +1,18 @@
+function calculateWinner(squares) {
+  const winLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 5],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6]
+  ];
+  for (const arr of winLines) {
+    const [a, b, c] = arr;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      return squares[a];
+  }
+}
+export default calculateWinner;
