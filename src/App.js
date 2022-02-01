4f652275642bbc4bc6ccc8efdc8310397a4d6058
SHEKHAR KUMMARI <74283917+SHEKHARKUMMARI@users.noreply.github.com>
2022-02-01 22:24:01 +0530

    updated styl

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./styles.css";
 import Game from "./Game";
 export default function App() {
   return (
-    <div className="App">
+    <div className="tc">
       <h1 className="tc">Tic-Tac-Toe</h1>
+      
       <Game />
+      
     </div>
   );
 }
